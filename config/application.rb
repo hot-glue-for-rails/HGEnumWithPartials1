877eef35765244f81b749c1734a93f71163dbc7c
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-10-01 07:31:57 -0400

    disables extraneous generators

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,22 @@ Bundler.require(*Rails.groups)
 
 module HGEnumWithPartials1
   class Application < Rails::Application
+	config.generators do |generate|
+      generate.helper false
+
+      generate.assets false
+      generate.helper false
+      generate.stylesheets false
+      generate.test_framework :rspec,
+                              request_specs: false,
+                              view_specs: false,
+                              controller_specs: false,
+                              helper_specs: false,
+                              routing_specs: false,
+                              fixture: false,
+                              fixture_replacement: "factory_bot"
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
